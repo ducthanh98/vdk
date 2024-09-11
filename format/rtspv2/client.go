566184b7d4cac4a1cec84b78a2d851e9baf04ba8
thanhld <thanhld9x.dev@gmail.com>
2024-09-11 16:27:18 +0700

    update

--- format/rtspv2/client.go
+++ format/rtspv2/client.go
@@ -258,7 +258,7 @@ func (client *RTSPClient) ControlTrack(track string) string {
 }
 
 func (client *RTSPClient) startStream() {
-	fmt.Println("hi hi hi ")
+	fmt.Println("ha ha ")
 	defer func() {
 		client.Println("Trigger close")
 		// client.Signals <- SignalStreamRTPStop
