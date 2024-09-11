0dd43157d6b995a21eebcc7dc20daaa01a6b73ee
thanhld <thanhld9x.dev@gmail.com>
2024-09-11 17:07:09 +0700

    update

--- format/rtspv2/client.go
+++ format/rtspv2/client.go
@@ -261,30 +261,12 @@ func (client *RTSPClient) startStream() {
 	fmt.Println("ha ha ")
 	defer func() {
 		client.Println("Trigger close")
-		// client.Signals <- SignalStreamRTPStop
+		client.Signals <- SignalStreamRTPStop
 	}()
 
 	reconnect := func() error {
 		client.Println("RTSP Client: Reconnecting...")
-		client.conn.Close() // Close the existing connection
-		*client = RTSPClient{
-			headers:             make(map[string]string),
-			Signals:             make(chan int, 100),
-			OutgoingProxyQueue:  make(chan *[]byte, 3000),
-			OutgoingPacketQueue: make(chan *av.Packet, 3000),
-			BufferRtpPacket:     bytes.NewBuffer([]byte{}),
-			videoID:             -1,
-			audioID:             -2,
-			videoIDX:            -1,
-			audioIDX:            -2,
-			options:             client.options,
-			AudioTimeScale:      8000,
-		}
-		client.headers["User-Agent"] = "Lavf58.76.100"
-		err := client.parseURL(html.UnescapeString(client.options.URL))
-		if err != nil {
-			return err
-		}
+		// client.conn.Close() // Close the existing connection
 		conn, err := net.DialTimeout("tcp", client.pURL.Host, client.options.DialTimeout)
 		if err != nil {
 			return err
@@ -303,99 +285,6 @@ func (client *RTSPClient) startStream() {
 		}
 		client.conn = conn
 		client.connRW = bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-		err = client.request(OPTIONS, nil, client.pURL.String(), false, false)
-		if err != nil {
-			return err
-		}
-		err = client.request(DESCRIBE, map[string]string{"Accept": "application/sdp"}, client.pURL.String(), false, false)
-		if err != nil {
-			return err
-		}
-		for _, i2 := range client.mediaSDP {
-			if (i2.AVType != VIDEO && i2.AVType != AUDIO) || (client.options.DisableAudio && i2.AVType == AUDIO) {
-				//TODO check it
-				if strings.Contains(string(client.SDPRaw), "LaunchDigital") {
-					client.chTMP += 2
-				}
-				continue
-			}
-			err = client.request(SETUP, map[string]string{"Transport": "RTP/AVP/TCP;unicast;interleaved=" + strconv.Itoa(client.chTMP) + "-" + strconv.Itoa(client.chTMP+1)}, client.ControlTrack(i2.Control), false, false)
-			if err != nil {
-				return err
-			}
-			if i2.AVType == VIDEO {
-				if i2.Type == av.H264 {
-					if len(i2.SpropParameterSets) > 1 {
-						if codecData, err := h264parser.NewCodecDataFromSPSAndPPS(i2.SpropParameterSets[0], i2.SpropParameterSets[1]); err == nil {
-							client.sps = i2.SpropParameterSets[0]
-							client.pps = i2.SpropParameterSets[1]
-							client.CodecData = append(client.CodecData, codecData)
-						}
-					} else {
-						client.CodecData = append(client.CodecData, h264parser.CodecData{})
-						client.WaitCodec = true
-					}
-					client.FPS = i2.FPS
-					client.videoCodec = av.H264
-				} else if i2.Type == av.H265 {
-					if len(i2.SpropVPS) > 1 && len(i2.SpropSPS) > 1 && len(i2.SpropPPS) > 1 {
-						if codecData, err := h265parser.NewCodecDataFromVPSAndSPSAndPPS(i2.SpropVPS, i2.SpropSPS, i2.SpropPPS); err == nil {
-							client.vps = i2.SpropVPS
-							client.sps = i2.SpropSPS
-							client.pps = i2.SpropPPS
-							client.CodecData = append(client.CodecData, codecData)
-						}
-					} else {
-						client.CodecData = append(client.CodecData, h265parser.CodecData{})
-					}
-					client.videoCodec = av.H265
-
-				} else {
-					client.Println("SDP Video Codec Type Not Supported", i2.Type)
-				}
-				client.videoIDX = int8(len(client.CodecData) - 1)
-				client.videoID = client.chTMP
-			}
-			if i2.AVType == AUDIO {
-				client.audioID = client.chTMP
-				var CodecData av.AudioCodecData
-				switch i2.Type {
-				case av.AAC:
-					CodecData, err = aacparser.NewCodecDataFromMPEG4AudioConfigBytes(i2.Config)
-					if err == nil {
-						client.Println("Audio AAC bad config")
-					}
-				case av.OPUS:
-					var cl av.ChannelLayout
-					switch i2.ChannelCount {
-					case 1:
-						cl = av.CH_MONO
-					case 2:
-						cl = av.CH_STEREO
-					default:
-						cl = av.CH_MONO
-					}
-					CodecData = codec.NewOpusCodecData(i2.TimeScale, cl)
-				case av.PCM_MULAW:
-					CodecData = codec.NewPCMMulawCodecData()
-				case av.PCM_ALAW:
-					CodecData = codec.NewPCMAlawCodecData()
-				case av.PCM:
-					CodecData = codec.NewPCMCodecData()
-				default:
-					client.Println("Audio Codec", i2.Type, "not supported")
-				}
-				if CodecData != nil {
-					client.CodecData = append(client.CodecData, CodecData)
-					client.audioIDX = int8(len(client.CodecData) - 1)
-					client.audioCodec = CodecData.Type()
-					if i2.TimeScale != 0 {
-						client.AudioTimeScale = int64(i2.TimeScale)
-					}
-				}
-			}
-			client.chTMP += 2
-		}
 		//test := map[string]string{"Scale": "1.000000", "Speed": "1.000000", "Range": "clock=20210929T210000Z-20210929T211000Z"}
 		err = client.request(PLAY, nil, client.control, false, false)
 		if err != nil {
@@ -417,7 +306,7 @@ func (client *RTSPClient) startStream() {
 			return
 		}
 
-		if int(time.Since(timer).Seconds()) > 25 {
+		if int(time.Since(timer).Seconds()) > 5 {
 			err := client.request(OPTIONS, map[string]string{"Require": "implicit-play"}, client.control, false, true)
 			if err != nil {
 				client.Println("RTSP Client RTP keep-alive", err)
